41b25ec0c16c0272b074b7495d8e64403cb65b57
Veerapaneni Koushik <koushikveerapaneni09@gmail.com>
2026-03-14 16:39:51 +0530

    Updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -13,15 +14,19 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// static folders
 app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 
 connectDB();
 
-app.use("/", userRoutes);
-app.use("/", adminRoutes);
-app.use("/", productRoutes);
+// ROUTES
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
+  console.log("Connected to DB");
 });
